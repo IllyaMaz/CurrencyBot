9634397d7f3cdc5a0d70a30a3a1a6a2055cd56e2
64erbet <fox2703@gmail.com>
2022-07-22 13:22:56 +0300

    For Ilya to review!!

--- src/main/java/MakeOutputString.java
+++ src/main/java/MakeOutputString.java
@@ -1,15 +1,15 @@
 import banksAPIparsing.*;
 import banksAPIparsing.BankResponse;
+
+import settings.*;
 import settings.BankSetting.*;
 import settings.CurrencySetting;
 import settings.CurrencySetting.*;
+
 import settings.NumberSimbolsAfterCommaSetting;
 
-import static settings.CurrencySetting.getSavedCurrencies;
 import java.io.IOException;
 import java.util.List;
-
-
 
 public class MakeOutputString {
     private String outputString = "";
@@ -26,24 +26,33 @@ public class MakeOutputString {
     }
 
     public void processInfo() {
-        CurrencySetting currencySetting = new CurrencySetting();
-        currencySetting.setSavedCurrency(1L,
-                CurrencySetting.Currency.valueOf("EUR"));
-        currencySetting.setSavedCurrency(1L,
-                CurrencySetting.Currency.valueOf("USD"));
-
-
-
-
-
         setChatId(1L);
+//****************************************************************************************
+//        CurrencySetting currencySetting = new CurrencySetting();
+//        currencySetting.setSavedCurrency(1L, Currency.valueOf("EUR"));
+//        currencySetting.setSavedCurrency(1L, Currency.valueOf("USD"));
+//
+//        NumberSimbolsAfterCommaSetting numberSimbolsAfterCommaSetting
+//                = new NumberSimbolsAfterCommaSetting();
+//        numberSimbolsAfterCommaSetting
+//                .setSimbolsAfterComma(1L, NumberSimbolsAfterCommaSetting
+//                        .NumberSimbolsAfterComma
+//                        .valueOf("FOUR"));
+//
+//        BankSetting bankSetting = new BankSetting();
+//        bankSetting.setSavedBank(1L, Bank.MONO);
+//****************************************************************************************
         List<Currency> selectedCurrencys = CurrencySetting.getSavedCurrencies(chatId);
-        System.out.println("Выбранные валюты: (List<Currency>) " + selectedCurrencys);
+        System.out.println("Выбранные валюты: " + selectedCurrencys);
         NumberSimbolsAfterCommaSetting.NumberSimbolsAfterComma afterComma =
                 NumberSimbolsAfterCommaSetting.getSimbolsAfterComma(chatId);
         System.out.println("Выбранное колличество знаков после запятой: " + afterComma);
-        String regular = "";
+        Bank selectedBank = BankSetting.getSavedBank(chatId);
+        System.out.println("Выбранный банк: " + selectedBank.name().toLowerCase());
+        System.out.println();
 
+
+        String regular = "";
         switch (afterComma){
             case TWO:
                 regular = "%.2f";
@@ -58,28 +67,23 @@ public class MakeOutputString {
         List<BankResponse> list;
         try {
             list = HTTPclient.getAllExchangeRates();
-
             for (BankResponse resp: list) {
                 for (Currency currency : selectedCurrencys) {
-                    if (resp.getCurrencyCode().equalsIgnoreCase(currency.name())) {
-                        System.out.println();
+                    if (resp.getCurrencyCode().equalsIgnoreCase(currency.name()) &&
+                        resp.getBankName().toLowerCase().contains(selectedBank.name().toLowerCase())) {
                         outputString += "Банк: " + resp.getBankName() + "\n";
                         outputString += "Валюта: " + resp.getCurrencyCode() + "\n";
                         outputString += "Продажа: " + String.format(regular, resp.getSell()) + "\n";
                         outputString += "Покупка: " + String.format(regular, resp.getBuy()) + "\n";
                         outputString += "\n";
-                        System.out.println();
                     }
                 }
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println("outputString = " + "\n\n" + outputString);
+        System.out.println(outputString);
     }
-
 }

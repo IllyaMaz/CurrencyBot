5af5cff998fd2935ce11c4b910281b426b469e1f
Sergii Shynkarenko <atlantiks@outlook.com>
2022-07-24 14:08:31 +0300

    HTTPclient: new API data is now compared with existing before re-write

--- src/main/java/banksAPIparsing/HTTPclient.java
+++ src/main/java/banksAPIparsing/HTTPclient.java
@@ -39,12 +39,11 @@ public class HTTPclient {
     }
 
     public static void updateAllExchangeRates() throws IOException, InterruptedException {
-        List<BankResponse> allLastUpdates = new ArrayList<>(20);
+        List<BankResponse> allLastUpdates = new ArrayList<>(ALL_RATES);
         addUpdate(getPrivatbankData(4),allLastUpdates);
         addUpdate(getPrivatbankData(3),allLastUpdates);
         addUpdate(getMonobankData(),allLastUpdates);
         addUpdate(getNBUData(),allLastUpdates);
-        System.out.println(allLastUpdates.size());
         synchronized (HTTPclient.class) {
             ALL_RATES.clear();
             ALL_RATES.addAll(allLastUpdates);
@@ -54,6 +53,7 @@ public class HTTPclient {
     private static <T extends BankResponse> void addUpdate(Optional<T[]> courses, List<BankResponse> list) {
         courses.ifPresent(currencyArray -> Arrays.stream(currencyArray)
                 .filter(currency -> Currencies.currs.containsKey(currency.getCurrencyCode()))
+                .peek(list::remove)
                 .forEach(list::add));
     }
 
